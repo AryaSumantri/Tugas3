1019b1ca8df94d8b3cb90a6278173948f70c376c
Aryadi Sumantri <147815087+AryaSumantri@users.noreply.github.com>
2023-10-13 16:49:49 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let bendera1 = "merah putih"
+let huruf_besar = bendera1.toUpperCase()
+
+let bendera2 = "merah putih"
+let huruf_kecil = bendera2.toLocaleLowerCase()
+
+let bendera3 = "merah putih"
+let ubah = bendera3.replace("putih", "muda")
+
+let bendera4 = "merah putih"
+let panjang = bendera4.length
+
+alert(huruf_besar)
+alert(huruf_kecil)
+alert(ubah)
+alert(panjang)
